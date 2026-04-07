5e9a7ef08acdd7cc593d258e9be12b2d053ac6a1
Khneliuk <arinakhmeluyk@gmail.com>
2026-04-07 13:21:09 +0300

    оновлено

--- task1/task1.cs
+++ task1/task1.cs
@@ -1,109 +1,116 @@
 using System;
+using System.Collections.Generic;
+
 namespace laba_4;
-    public class City
-	{
-		public string Name;
-		public int Population;
-		public City(string name, int population)
-		{
-            this.Name = name;
-            this.Population = population;
-            Console.WriteLine("створено місто: " + name);
-        }
-		public override string ToString()
-        {
-            return "місто " + Name + " (населення: " + Population + ")";
-        }
-        public override bool Equals(object? obj)
-        {
-            if (obj is City other)
-			{
-				return this.Name == other.Name;
-			}
-            return false;
-        }
-        public override int GetHashCode()
-        {
-            return Name.GetHashCode();
-        }
-	}
-	public class District
+
+public class City
+{
+    public string Name;
+    public int Population;
+
+    public City(string name, int population)
     {
-        public string Name;
-        public District(string name)
-        {
-            this.Name = name;
-            Console.WriteLine("створено район: " + name);
-        }
+        this.Name = name;
+        this.Population = population;
     }
-	public class Region
+
+    public override string ToString() => $"{Name} (населення: {Population})";
+
+    public override bool Equals(object? obj)
     {
-        public string Name;
-        public double Area;
-        public City Center; 
-        public Region(string name, double area, City center)
-        {
-            this.Name = name;
-            this.Area = area;
-            this.Center = center;
-            Console.WriteLine("створено область: " + name);
-        }
+        if (obj is City other) return this.Name == other.Name;
+        return false;
     }
-	public class Country
+
+    public override int GetHashCode() => Name.GetHashCode();
+}
+
+public class District
+{
+    public string Name;
+    public City[] Cities;
+
+    public District(string name, City[] cities)
     {
-        public string Name;
-        public City Capital;
-        public Region[] Regions; 
-        public Country(string name, City capital, Region[] regions)
-        {
-            this.Name = name;
-            this.Capital = capital;
-            this.Regions = regions;
-            Console.WriteLine("створено об'єкт Держава: " + name);
-        }
-        public void printCapital()
-        {
-            Console.WriteLine("столиця: " + Capital);
-        }
-        public void printRegionsCount()
-        {
-            Console.WriteLine("кількість областей: " + Regions.Length);
-        }
-        public void printTotalArea()
-        {
-            double total = 0;
-            foreach (Region r in Regions) total += r.Area;
-            Console.WriteLine("загальна площа: " + total + " кв. км");
-        }
-        public void printCenters()
-        {
-            Console.WriteLine("обласні центри:");
-            foreach (Region r in Regions)
-            {
-                Console.WriteLine("- " + r.Center);
-            }
-        }
-	}
+        this.Name = name;
+        this.Cities = cities;
+        Console.WriteLine("створено район: " + name);
+    }
+}
+
+public class Region
+{
+    public string Name;
+    public double Area;
+    public City Center;
+    public District[] Districts;
+
+    public Region(string name, double area, City center, District[] districts)
+    {
+        this.Name = name;
+        this.Area = area;
+        this.Center = center;
+        this.Districts = districts;
+        Console.WriteLine("створено область: " + name);
+    }
+}
+
+public class Country
+{
+    public string Name;
+    public City Capital;
+    public Region[] Regions;
+
+    public Country(string name, City capital, Region[] regions)
+    {
+        this.Name = name;
+        this.Capital = capital;
+        this.Regions = regions;
+        Console.WriteLine("створено об'єкт Держава: " + name);
+    }
+
+    public void printCapital() => Console.WriteLine("столиця: " + Capital);
+    public void printRegionsCount() => Console.WriteLine("кількість областей: " + Regions.Length);
+
+    public void printTotalArea()
+    {
+        double total = 0;
+        foreach (Region r in Regions) total += r.Area;
+        Console.WriteLine("загальна площа: " + total + " кв. км");
+    }
+
+    public void printCenters()
+    {
+        Console.WriteLine("обласні центри:");
+        foreach (Region r in Regions) Console.WriteLine("- " + r.Center);
+    }
+}
 
 class Program
+{
+    static void Main()
     {
-        static void Main()
-        {
-            City kyiv = new City("Київ", 3600000);
-            City lviv = new City("Львів", 1000000);
-            City odessa = new City("Одеса", 1000000);
-            
-            Region r1 = new Region("Київська", 28131, kyiv);
-            Region r2 = new Region("Львівська", 21833, lviv);
-            Region r3 = new Region("Одеська", 33314, odessa);
+        City kyiv = new City("Київ", 3600000);
+        City bilaTserkva = new City("Біла Церква", 208000);
+        City lviv = new City("Львів", 720000);
+        City odessa = new City("Одеса", 1010000);
 
-            Country сountry = new Country("Україна", kyiv, new Region[] { r1, r2, r3 });
+        District d1 = new District("Білоцерківський", new City[] { bilaTserkva });
+        District d2 = new District("Київський", new City[] { kyiv });
+        District d3 = new District("Львівський", new City[] { lviv });
+        District d4 = new District("Одеський", new City[] { odessa });
 
-            сountry.printCapital();
-            сountry.printRegionsCount();
-            сountry.printTotalArea();
-            сountry.printCenters();
+        Region r1 = new Region("Київська", 28131, kyiv, new District[] { d1, d2 });
+        Region r2 = new Region("Львівська", 21833, lviv, new District[] { d3 });
+        Region r3 = new Region("Одеська", 33314, odessa, new District[] { d4 });
 
-            Console.ReadLine();
-        }
+        Country country = new Country("Україна", kyiv, new Region[] { r1, r2, r3 });
+
+        country.printCapital();
+        country.printRegionsCount();
+        country.printTotalArea();
+        country.printCenters();
+
+        Console.ReadLine();
     }
+}
